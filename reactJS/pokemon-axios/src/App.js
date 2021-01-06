e9aab411422b7404bdab739dc56803d551adcd51
Krystine Xie <krystinex@gmail.com>
2021-01-06 15:53:57 -0800

    pokemon axios

---
+++ reactJS/pokemon-axios/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import PokeApi from './components/PokeApi';
+
+function App() {
+  return (
+    <div className="App">
+      <PokeApi />
+    </div>
+  );
+}
+
+export default App;
---
+++ reactJS/pokemon-axios/src/components/PokeApi.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const PokeApi = (props) => {
+    const [pokemon, setPokemon] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        axios.get('https://pokeapi.co/api/v2/pokemon/?limit=807')
+            .then(response => {
+                setPokemon(response.data.results)
+                console.log(response);
+            })
+            
+    }, []);
+
+    const displayPokemon = () => {
+        setLoaded(true); 
+    }
+
+    return (
+        <div className="wrapper">
+            <button onClick={displayPokemon}>Fetch Pokemon</button>
+            <ul>
+                {loaded && pokemon.map((creature, index) => {
+                    return (
+                        <li key={index}>{creature.name}</li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default PokeApi;
