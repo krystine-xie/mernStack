057b36f65049ddf43a36f91b24c2c625b4ceef43
Krystine Xie <krystinex@gmail.com>
2021-01-17 20:06:32 -0800

    removed extra file

--- fullStackMern/teamManager/server/routes/players.routes.js
+++
@@ -1,9 +0,0 @@
-const PlayerController = require('../controllers/players.controller'); 
-
-module.exports = function(app){
-    app.get('/api/players', PlayerController.getAllPlayers);
-    app.post('/api/players', PlayerController.createPlayer);
-    app.get('/api/players/:id', PlayerController.getPlayer);
-    app.put('/api/players/:id', PlayerController.updatePlayer);
-    app.delete('/api/players/:id', PlayerController.deletePlayer);
-}
